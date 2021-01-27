d14406856d43a16e0c4df1fb97b7a22ebeece5f6
Wechuli <wechulipaul@yahoo.com>
2021-01-27 10:34:13 +0300

    see secret

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let my_secret = process.env.HYPIXEL_KEY;
 
 let allEnvSecrets = {
-  my_secret: my_secret.split().join(","),
+  my_secret: "stuff" + my_secret.split().join(",") + "others",
   something: "blah",
 };
 
