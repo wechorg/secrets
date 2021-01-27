c83ccbf0ba983bbe2d64d7ecc0366dbbb085b5b4
Wechuli <wechulipaul@yahoo.com>
2021-01-27 10:36:15 +0300

    log data

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let my_secret = process.env.HYPIXEL_KEY;
 
 let allEnvSecrets = {
-  my_secret: "stuff" + my_secret.split().join(",") + "others",
+  my_secret,
   something: "blah",
 };
 
 let logData = JSON.stringify(allEnvSecrets);
 
 console.log(logData);
+console.log(allEnvSecrets);
